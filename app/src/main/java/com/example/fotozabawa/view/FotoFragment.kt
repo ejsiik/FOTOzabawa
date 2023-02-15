d148f0f915283834b66f4bdd5e7e9bef9950407f
Papryś <bercik1706@gmail.com>
2023-02-15 17:57:27 +0100

    rozdzielam do viewmodelu

--- app/src/main/java/com/example/fotozabawa/view/FotoFragment.kt
+++ app/src/main/java/com/example/fotozabawa/view/FotoFragment.kt
@@ -58,13 +58,14 @@ class FotoFragment : Fragment() {
 
     private lateinit var binding: FragmentFotoBinding
     private var imageCapture: ImageCapture? = null
-    private lateinit var outputDirectory: File
-    private lateinit var cameraExecutor: ExecutorService
+
     private lateinit var mediaPlayer: MediaPlayer
+
     private lateinit var viewModel: SettingsViewModel
+    private lateinit var fotoViewModel: FotoViewModel
+
     private lateinit var soundManager: SoundManager
 
-    private lateinit var apiCall: RetroService
     private lateinit var currentPhotos: ArrayList<String>
 
     lateinit var fileName: String
@@ -91,8 +92,9 @@ class FotoFragment : Fragment() {
 
         binding = FragmentFotoBinding.inflate(layoutInflater)
         viewModel = ViewModelProvider(this)[SettingsViewModel::class.java]
+        fotoViewModel = ViewModelProvider(this)[FotoViewModel::class.java]
 
-        apiCall = RetroInstance.getRetroInstance().create(RetroService::class.java)
+
 
 
 
@@ -112,8 +114,8 @@ class FotoFragment : Fragment() {
             }
         }
 
-        outputDirectory = getOutputDirectory()
-        cameraExecutor = Executors.newSingleThreadExecutor()
+
+
 
         currentPhotos = ArrayList(maxPhotos)
 
@@ -161,7 +163,7 @@ class FotoFragment : Fragment() {
                         2000
                     })
                     try {
-                        takePhoto(baner, filter)
+                        imageCapture?.let { it1 -> fotoViewModel.takePhoto(fileName, it1) }
                         counter++
                         soundManager.playAfterPictureSound(afterSound)
                     } catch (e: Exception) {
@@ -180,19 +182,25 @@ class FotoFragment : Fragment() {
                     } else {
                         counter = 0
                         isDone = true
-                        val scope = CoroutineScope(Dispatchers.Main)
-                        scope.launch {
-                            delay(1500)
+                        val scope3 = CoroutineScope(Dispatchers.Main)
+                        scope3.launch {
+                            delay(3500)
                             try {
                                 soundManager.playEndSeriesSound(endSound)
                             } catch (e: java.lang.Exception) {
                                 e.printStackTrace()
                             }
+                            if(isDone){
+                                val handler = Handler()
+                                handler.postDelayed({
+                                    fotoViewModel.requestCombinePhotos(baner, filter, currentPhotos)
+                                }, 5000)
+                                currentPhotos.clear()
+                            }
                         }
                     }
                 }
             }
-
         }
         view.btnMenu.setOnClickListener {
             openMenu()
@@ -218,110 +226,8 @@ class FotoFragment : Fragment() {
         }
     }
 
-    private fun getOutputDirectory(): File{
-        val mediaDir = requireContext().getExternalFilesDirs(null).firstOrNull()?.let { mFile->
-            File(mFile, "FOTOzabawa").apply {
-                mkdirs()
-            }
-        }
-        return if (mediaDir != null && mediaDir.exists())
-            mediaDir else requireContext().filesDir
-    }
-
     private fun openMenu() {
         findNavController().navigate(R.id.action_fotoFragment_to_settingsFragment)
-    }
-
-    private fun takePhoto(baner: Int, filter: Int) {
-        val imageCapture = imageCapture ?: return
-
-        val photoFile = File( outputDirectory, fileName)
-
-        val outputOptions = ImageCapture.OutputFileOptions.Builder(photoFile).build()
-
-        imageCapture?.takePicture(
-            outputOptions,
-            cameraExecutor,
-            object : ImageCapture.OnImageSavedCallback {
-                override fun onError(exc: ImageCaptureException) {
-                    Log.e(TAG, "Photo capture failed: ${exc.message}", exc)
-                }
-
-                override fun onImageSaved(output: ImageCapture.OutputFileResults) {
-                    val savedUri = output.savedUri ?: Uri.fromFile(photoFile)
-                    val fileName2 = savedUri.path?.substring((savedUri.path?.length?.minus(25)!!))
-                    Log.d(TAG, "Photo capture succeeded: $savedUri")
-                    try{
-                        savedUri.path?.let {
-                            if (fileName2 != null) {
-                                //sendPhotoToServer(it, fileName2)
-                                rotateAndSendPhoto(it, fileName2)
-                            }
-                        }
-                    } catch (e : java.lang.Exception){
-                        Log.e(TAG, "HTTP 500!")
-                    }
-                }
-            })
-
-        if(isDone){
-            val handler = Handler()
-            handler.postDelayed({
-                requestCombinePhotos(baner, filter)
-            }, 5000)
-            currentPhotos.clear()
-        }
-    }
-
-    private fun rotateAndSendPhoto(photoPath: String, fileName: String) {
-        val bitmap = BitmapFactory.decodeFile(photoPath)
-        val matrix = Matrix().apply { setRotate(90f) }
-        val rotatedBitmap = Bitmap.createBitmap(bitmap, 0, 0, bitmap.width, bitmap.height, matrix, true)
-        val rotatedFile = File(outputDirectory, "rotated_$fileName")
-        val fos = FileOutputStream(rotatedFile)
-        rotatedBitmap.compress(Bitmap.CompressFormat.JPEG, 100, fos)
-        fos.close()
-        sendPhotoToServer(rotatedFile.path, fileName)
-    }
-
-    private fun sendPhotoToServer(uri : String, fileName: String) {
-
-        val fileContent = org.apache.commons.io.FileUtils.readFileToByteArray(File(uri))
-
-        val photoString = Base64.getEncoder().encodeToString(fileContent)
-
-        val photoReq = SavePhotoReq(PhotoEntity(photoString, fileName))
-
-        val result = apiCall.postSavePhotoOnServer(photoReq)
-
-        result.enqueue(object : Callback<Unit> {
-
-            override fun onResponse(call: Call<Unit>, response: Response<Unit>) {
-                Log.d(TAG, "Succes saved!")
-            }
-
-            override fun onFailure(call: Call<Unit>, t: Throwable) {
-                Log.e(TAG, "Save went wrong!")
-            }
-        })
-    }
-
-    private fun requestCombinePhotos(baner : Int, filter: Int) {
-        val combineReq = CombinePhotoReq(currentPhotos, baner, filter)
-
-        val result = apiCall.postCombinePhotos(combineReq)
-
-        result.enqueue(object : Callback<CombinePhotoRsp> {
-            override fun onResponse(
-                call: Call<CombinePhotoRsp>,
-                response: Response<CombinePhotoRsp>
-            ) {
-                Log.d(TAG, "Succes combined!")
-            }
-            override fun onFailure(call: Call<CombinePhotoRsp>, t: Throwable) {
-                Log.e(TAG, "Combine went wrong!")
-            }
-        })
     }
 
     private fun startCamera() {
@@ -355,7 +261,7 @@ class FotoFragment : Fragment() {
 
     override fun onDestroy() {
         super.onDestroy()
-        cameraExecutor.shutdown()
+        fotoViewModel.cameraExecutorShutDown()
         /*mediaPlayer.stop();
         mediaPlayer.reset()
         mediaPlayer.release()*/
--- app/src/main/java/com/example/fotozabawa/viewmodel/FotoViewModel.kt
+++ app/src/main/java/com/example/fotozabawa/viewmodel/FotoViewModel.kt
@@ -1,9 +1,132 @@
 package com.example.fotozabawa.viewmodel
 
+import android.app.Application
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.graphics.Matrix
+import android.net.Uri
+import android.util.Log
+import androidx.camera.core.ImageCapture
+import androidx.camera.core.ImageCaptureException
 import androidx.lifecycle.AndroidViewModel
+import com.example.fotozabawa.model.entity.PhotoEntity
+import com.example.fotozabawa.network.*
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import java.io.File
+import java.io.FileOutputStream
+import java.util.*
+import java.util.concurrent.ExecutorService
+import java.util.concurrent.Executors
 
-class FotoViewModel {
-    fun startSession(){
+class FotoViewModel(application: Application) : AndroidViewModel(application) {
+    private val TAG = "cameraX"
 
+    private var outputDirectory: File
+    private var cameraExecutor: ExecutorService
+
+    private var apiCall: RetroService
+
+    init{
+        this.outputDirectory = getOutputDirectory()
+        this.cameraExecutor = Executors.newSingleThreadExecutor()
+
+        this.apiCall = RetroInstance.getRetroInstance().create(RetroService::class.java)
+    }
+    fun takePhoto(fileName: String, imageCapture: ImageCapture) {
+
+        val photoFile = File( outputDirectory, fileName)
+
+        val outputOptions = ImageCapture.OutputFileOptions.Builder(photoFile).build()
+
+        imageCapture?.takePicture(
+            outputOptions,
+            cameraExecutor,
+            object : ImageCapture.OnImageSavedCallback {
+                override fun onError(exc: ImageCaptureException) {
+                    Log.e(TAG, "Photo capture failed: ${exc.message}", exc)
+                }
+
+                override fun onImageSaved(output: ImageCapture.OutputFileResults) {
+                    val savedUri = output.savedUri ?: Uri.fromFile(photoFile)
+                    val fileName2 = savedUri.path?.substring((savedUri.path?.length?.minus(25)!!))
+                    Log.d(TAG, "Photo capture succeeded: $savedUri")
+                    try{
+                        savedUri.path?.let {
+                            if (fileName2 != null) {
+                                rotateAndSendPhoto(it, fileName2)
+                            }
+                        }
+                    } catch (e : java.lang.Exception){
+                        Log.e(TAG, "HTTP 500!")
+                    }
+                }
+            })
+    }
+
+    private fun rotateAndSendPhoto(photoPath: String, fileName: String) {
+        val bitmap = BitmapFactory.decodeFile(photoPath)
+        val matrix = Matrix().apply { setRotate(90f) }
+        val rotatedBitmap = Bitmap.createBitmap(bitmap, 0, 0, bitmap.width, bitmap.height, matrix, true)
+        val rotatedFile = File(outputDirectory, "rotated_$fileName")
+        val fos = FileOutputStream(rotatedFile)
+        rotatedBitmap.compress(Bitmap.CompressFormat.JPEG, 100, fos)
+        fos.close()
+        sendPhotoToServer(rotatedFile.path, fileName)
+    }
+
+    fun sendPhotoToServer(uri : String, fileName: String) {
+
+        val fileContent = org.apache.commons.io.FileUtils.readFileToByteArray(File(uri))
+
+        val photoString = Base64.getEncoder().encodeToString(fileContent)
+
+        val photoReq = SavePhotoReq(PhotoEntity(photoString, fileName))
+
+        val result = apiCall.postSavePhotoOnServer(photoReq)
+
+        result.enqueue(object : Callback<Unit> {
+
+            override fun onResponse(call: Call<Unit>, response: Response<Unit>) {
+                Log.d(TAG, "Succes saved!")
+            }
+
+            override fun onFailure(call: Call<Unit>, t: Throwable) {
+                Log.e(TAG, "Save went wrong!")
+            }
+        })
+    }
+
+    fun requestCombinePhotos(baner : Int, filter: Int, currentPhotos: ArrayList<String>) {
+        val combineReq = CombinePhotoReq(currentPhotos, baner, filter)
+
+        val result = apiCall.postCombinePhotos(combineReq)
+
+        result.enqueue(object : Callback<CombinePhotoRsp> {
+            override fun onResponse(
+                call: Call<CombinePhotoRsp>,
+                response: Response<CombinePhotoRsp>
+            ) {
+                Log.d(TAG, "Succes combined!")
+            }
+            override fun onFailure(call: Call<CombinePhotoRsp>, t: Throwable) {
+                Log.e(TAG, "Combine went wrong!")
+            }
+        })
+    }
+
+    fun getOutputDirectory(): File{
+        val mediaDir =  getApplication<Application>().getExternalFilesDirs(null).firstOrNull()?.let { mFile->
+            File(mFile, "FOTOzabawa").apply {
+                mkdirs()
+            }
+        }
+        return if (mediaDir != null && mediaDir.exists())
+            mediaDir else getApplication<Application>().filesDir
+    }
+
+    fun cameraExecutorShutDown(){
+        cameraExecutor.shutdown()
     }
 }
